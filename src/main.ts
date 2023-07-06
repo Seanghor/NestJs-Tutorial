476648ae884f5928b1698fa56364a9f3a540b00d
Seanghor <haiseanghor20@kit.edu.kh>
2023-07-06 16:48:54 +0700

    fix-db

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
   // Enable CORS
   app.use(cors());
 
-  const port = process.env.PORT || 8080;
+  const port = process.env.PORT || 4000;
   await app.listen(port);
   console.log(`Application is running on: ${port}`);
 }
